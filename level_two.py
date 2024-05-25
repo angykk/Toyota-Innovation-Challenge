17120c0601299bcb4065ac8552c8dbf4d9f86d6f
E. L. Raby <eleoraby@gmail.com>
2024-05-25 11:56:30 -0400

    Added More Logging

--- level_two.py
+++ level_two.py
@@ -1,18 +1,20 @@
 import TMMC_Wrapper
 import rclpy
-
 #Start Process
 def checkForStopSign(self,image):
     """Takes, self and an NP image and returns whether or not a stopsign in is in view"""
-    print("starting SS search")
-    image = self.red_filter(image)
-    image = self.add_contour(image)
-    model = YOLO('yolo8n.pt')
-    isStopSign = self.ML_predict_stop_sign(model,image)
+    try:   
+        print("starting SS search")
+        image = self.red_filter(image)
+        image = self.add_contour(image)
+        model = YOLO('yolo8n.pt')
+        isStopSign = self.ML_predict_stop_sign(model,image)
 
-    if isStopSign:
-        print("stop sign")
-        return True
-    else:
-        print("no stop sign")
-        return False
+        if isStopSign:
+            print("stop sign")
+            return True
+        else:
+            print("no stop sign")
+            return False
+    except Exception as err:
+        print (err)
