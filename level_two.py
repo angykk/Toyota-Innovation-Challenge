94a0e2a57a4b99fccf3896fdfbb85c368e05bec4
E. L. Raby <eleoraby@gmail.com>
2024-05-25 11:46:25 -0400

    Implemented Stop Sign Dectection Function

--- level_two.py
+++ level_two.py
@@ -1,35 +1,18 @@
 import TMMC_Wrapper
 import rclpy
 
-#start ros
-if not rclpy.ok():
-    rclpy.init()
-
-TMMC_Wrapper.is_SIM = True
-if not TMMC_Wrapper.is_SIM:
-    #specify hardware api
-    TMMC_Wrapper.use_hardware()
-    
-if not "robot" in globals():
-    robot = TMMC_Wrapper.Robot()
-
-#debug messaging 
-print("running level_two: Stop Sign")
-
 #Start Process
-try:
-    while True:
-        robot.checkScan()
-        robot.checkImage()
-        currentImg = robot.rosImg_to_cv2()
-        currentImg = robot.red_filter(currentImg)
-        currentImg = robot.add_contour(currentImg)
-        model = YOLO('yolo8n.pt')
-        isStopSign = robot.ML_predict_stop_sign(model,currentImg)
+def checkForStopSign(self,image):
+    """Takes, self and an NP image and returns whether or not a stopsign in is in view"""
+    print("starting SS search")
+    image = self.red_filter(image)
+    image = self.add_contour(image)
+    model = YOLO('yolo8n.pt')
+    isStopSign = self.ML_predict_stop_sign(model,image)
 
-        if isStopSign:
-            print("stop sign")
-        else:
-            print("no stop sign")
-except KeyboardInterrupt:
-    print("keyboard interrupt receieved.Stopping...")
+    if isStopSign:
+        print("stop sign")
+        return True
+    else:
+        print("no stop sign")
+        return False
