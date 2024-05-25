8cf500e514b2654d97c35144ffa43263a6dd4854
Angela Huang <angelaykhuang@gmail.com>
2024-05-25 16:29:30 -0400

    lvl 1

--- level_one.py
+++ level_one.py
@@ -52,7 +52,7 @@ try:
 
         dist , _ = robot.detect_obstacle(robot.checkScan().ranges)
 
-        if(dist < 0.09):
+        #if(dist < 0.09):
             #robot.set_cmd_vel(0.0,0.5,3)
             # print("back")
             # robot.set_cmd_vel(-0.25,0,1)
