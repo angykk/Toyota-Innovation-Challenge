cafc7fb27d55ce91e0b85ab7e6f3dc791db6f42d
Yiting <YITINGHAN4@GMAIL.COM>
2024-05-25 13:12:54 -0400

    lvl1

--- level_one.py
+++ level_one.py
@@ -41,7 +41,7 @@ try:
         # print("C",rosImg_to_cv2())
         # checkForStopSigns(rosImg_to_cv2())
 
-        if((robot.detect_obstacle(robot.checkScan().ranges)[0]) < 0.3):
+        if((robot.detect_obstacle(robot.checkScan().ranges)[0]) < 0.1):
             print("back")
             set_cmd_vel(0.25,0,1)
             print("wait")
