3816289ccd396507550491ffd863652966880463
Angela Huang <angelaykhuang@gmail.com>
2024-05-25 14:42:50 -0400

    lvl 1

--- level_one.py
+++ level_one.py
@@ -39,6 +39,7 @@ try:
         rclpy.spin_once(robot, timeout_sec=0.1)
 
         image = robot.rosImg_to_cv2()
+        print(image)
         model = YOLO('yolov8n.pt')
         
         if robot.ML_predict_stop_sign(model,image):
