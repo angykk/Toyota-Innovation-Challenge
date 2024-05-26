8404a1b60ec6df2b56720e41120793ad8ff253f8
ang <angelak1516@gmail.com:wdquitequit>
2024-05-26 09:36:58 -0400

    level three debug

--- level_three.py
+++ level_three.py
@@ -36,7 +36,7 @@ try:
     while True:
         if((robot.detect_obstacle(robot.checkScan().ranges)[0]) > 0.2):
             robot.set_cmd_vel(0.25,0,0.5)
-        while((robot.detect_obstacle(robot.checkScan().ranges)[0]) < 0.2):
+        while((robot.detect_obstacle(robot.checkScan().ranges)[0]) < 0.02):
             print("turning")
             robot.set_cmd_vel(0,0.1,0.5)
 
