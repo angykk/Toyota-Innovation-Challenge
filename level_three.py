c5a3045ee9420fd7842c40361c2efae5396ee016
ang <angelak1516@gmail.com:wdquitequit>
2024-05-26 10:28:45 -0400

    detection collision

--- level_three.py
+++ level_three.py
@@ -34,6 +34,16 @@ rclpy.spin_once(robot, timeout_sec=0.1)
 try:
     print("Entering the robot loop which cycles until the script is stopped")
     while True:
+        # #start stop sign code
+        # image = robot.rosImg_to_cv2()
+        # model = YOLO('yolov8n.pt')
+        # stop_status = robot.ML_predict_stop_sign(model,image)
+        
+        # if (stop_status[0]):
+        #     print("stop sign detected")
+        #     robot.set_cmd_vel(0,0,1)
+        # #end stop sign code
+
         if(robot.detect_obstacle(robot.checkScan().ranges)[0] == -1):
             robot.set_cmd_vel(0.5,0,1)
 
